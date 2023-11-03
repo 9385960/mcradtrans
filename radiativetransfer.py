33dba4084b73583fd7204ba44d829b2b028eb9b0
Nikolas <n.jauch@ymail.com>
2023-11-03 12:01:54 -0400

    Implemented Some appendix functions

---
+++ radiativetransfer.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def weight(tau_a_tot):
+    return np.exp(-tau_a_tot)
+
+def tau_s(p):
+    return -np.log(p)
+
+def phi(theta,g):
+    num = (1/(4*np.pi))(1-g**2)
+    denom = (1+g**2-2*g*np.cos(theta))**(3/2)
+    return num/denom
+
+def theta(p,g):
+    num = (1+g**2)-np.abs((1-g**2)/(1-g+2*g*p))**2
+    denom = 2*g
+    return num/denom
+
